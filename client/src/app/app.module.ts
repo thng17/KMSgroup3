8b1edeabe2e7b735179b9d6a7957201b76244d55
thng17 <hangngtech@gmail.com>
2022-04-30 01:59:54 +0200

    add some dependencies for bootstrap

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
 import { EditTaskComponent } from './components/edit-task/edit-task.component';
 import { TaskCardComponent } from './components/task-card/task-card.component';
 import { TasksListComponent } from './components/tasks-list/tasks-list.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { TasksListComponent } from './components/tasks-list/tasks-list.component
     TasksListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
