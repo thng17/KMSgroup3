a11a22db0283777d141f8492bc70c34e9643562a
thng17 <hangngtech@gmail.com>
2022-04-30 01:26:12 +0200

    add server.ts file

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { EditTaskComponent } from './components/edit-task/edit-task.component';
+import { TaskCardComponent } from './components/task-card/task-card.component';
+import { TasksListComponent } from './components/tasks-list/tasks-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent,
+    EditTaskComponent,
+    TaskCardComponent,
+    TasksListComponent
   ],
   imports: [
     BrowserModule
